12e2fb529e074dff3191b6b2af2bf44110fbf7d1
sudoNameZen <imdewangannitin56@gmail.com>
2026-04-29 23:50:19 +0530

    updating and installing react-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Footer from "@/Component/shared/Footer";
 import Hero from "@/Component/ui/Hero";
 import Navbar from "@/Component/shared/Navbar";
+import Sidebar from "@/Component/shared/Sidebar";
 
 // sudozen logs
 export default function Home() {
   return (
     <>
     <Navbar />
-    <Hero/>
+    <Sidebar/>
     <Footer/>
     </>
   );
